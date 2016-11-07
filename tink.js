78f9c88772ee13bc3ee7db61e34fee54d1991b13
aaron <aahuang@s.sfusd.edu>
2016-11-07 00:08:56 -0800

    major edits with tink.js

--- tink.js
+++ tink.js
@@ -22,10 +22,6 @@ var Tink = (function () {
     //(there will usually just be one)
     this.pointers = [];
 
-    //An array to store all the buttons and button-like
-    //interactive sprites
-    this.buttons = [];
-
     //A local PIXI reference
     this.PIXI = PIXI;
 
@@ -93,26 +89,14 @@ var Tink = (function () {
       }
 
       //If the first argument isn't an array of sprites...
-      if (!(sprites[0] instanceof Array)) {
+      
         sprites.forEach(function (sprite) {
           _this2.draggableSprites.splice(_this2.draggableSprites.indexOf(sprite), 1);
           if (sprite._localDraggableAllocation === true) sprite.draggable = false;
-        });
-      }
-
-      //If the first argument is an array of sprites
-      else {
-          var spritesArray = sprites[0];
-          if (spritesArray.length > 0) {
-            for (var i = spritesArray.length - 1; i >= 0; i--) {
-              var sprite = spritesArray[i];
-              this.draggableSprites.splice(this.draggableSprites.indexOf(sprite), 1);
-              if (sprite._localDraggableAllocation === true) sprite.draggable = false;
-            }
-          }
-        }
+        });   
     }
-  }, {
+  },
+  {
     key: "makePointer",
     value: function makePointer() {
       var element = arguments.length <= 0 || arguments[0] === undefined ? this.element : arguments[0];
@@ -474,21 +458,6 @@ var Tink = (function () {
                 //Set the sprite as the pointer's `dragSprite` property
                 pointer.dragSprite = sprite;
 
-                //The next two lines re-order the `sprites` array so that the
-                //selected sprite is displayed above all the others.
-                //First, splice the sprite out of its current position in
-                //its parent's `children` array
-                var children = sprite.parent.children;
-                children.splice(children.indexOf(sprite), 1);
-
-                //Next, push the `dragSprite` to the end of its `children` array so that it's
-                //displayed last, above all the other sprites
-                children.push(sprite);
-
-                //Reorganize the `draggableSpites` array in the same way
-                draggableSprites.splice(draggableSprites.indexOf(sprite), 1);
-                draggableSprites.push(sprite);
-
                 //Break the loop, because we only need to drag the topmost sprite
                 break;
               }
@@ -568,317 +537,13 @@ var Tink = (function () {
     //inside the game loop. It updates all the button-like sprites
 
   }, {
-    key: "updateButtons",
-    value: function updateButtons() {
-      var _this3 = this;
-
-      //Create a pointer if one doesn't already exist
-      if (this.pointers.length === 0) {
-        this.makePointer(this.element, this.scale);
-      }
-
-      //Loop through all the button-like sprites that were created
-      //using the `makeInteractive` method
-      this.buttons.forEach(function (o) {
-
-        //Only do this if the interactive object is enabled
-        if (o.enabled) {
-
-          //Loop through all of Tink's pointers (there will usually
-          //just be one)
-          _this3.pointers.forEach(function (pointer) {
-
-            //Figure out if the pointer is touching the sprite
-            var hit = pointer.hitTestSprite(o);
-
-            //1. Figure out the current state
-            if (pointer.isUp) {
-
-              //Up state
-              o.state = "up";
-
-              //Show the first image state frame, if this is a `Button` sprite
-              if (o.tinkType === "button") o.gotoAndStop(0);
-            }
-
-            //If the pointer is touching the sprite, figure out
-            //if the over or down state should be displayed
-            if (hit) {
-
-              //Over state
-              o.state = "over";
-
-              //Show the second image state frame if this sprite has
-              //3 frames and it's a `Button` sprite
-              if (o.totalFrames && o.totalFrames === 3 && o.tinkType === "button") {
-                o.gotoAndStop(1);
-              }
-
-              //Down state
-              if (pointer.isDown) {
-                o.state = "down";
-
-                //Show the third frame if this sprite is a `Button` sprite and it
-                //has only three frames, or show the second frame if it
-                //only has two frames
-                if (o.tinkType === "button") {
-                  if (o.totalFrames === 3) {
-                    o.gotoAndStop(2);
-                  } else {
-                    o.gotoAndStop(1);
-                  }
-                }
-              }
-
-              //Change the pointer icon to a hand
-              if (pointer.visible) pointer.cursor = "pointer";
-            } else {
-              //Turn the pointer to an ordinary arrow icon if the
-              //pointer isn't touching a sprite
-              if (pointer.visible) pointer.cursor = "auto";
-            }
-
-            //Perform the correct interactive action
-
-            //a. Run the `press` method if the sprite state is "down" and
-            //the sprite hasn't already been pressed
-            if (o.state === "down") {
-              if (!o.pressed) {
-                if (o.press) o.press();
-                o.pressed = true;
-                o.action = "pressed";
-              }
-            }
-
-            //b. Run the `release` method if the sprite state is "over" and
-            //the sprite has been pressed
-            if (o.state === "over") {
-              if (o.pressed) {
-                if (o.release) o.release();
-                o.pressed = false;
-                o.action = "released";
-                //If the pointer was tapped and the user assigned a `tap`
-                //method, call the `tap` method
-                if (pointer.tapped && o.tap) o.tap();
-              }
-
-              //Run the `over` method if it has been assigned
-              if (!o.hoverOver) {
-                if (o.over) o.over();
-                o.hoverOver = true;
-              }
-            }
-
-            //c. Check whether the pointer has been released outside
-            //the sprite's area. If the button state is "up" and it's
-            //already been pressed, then run the `release` method.
-            if (o.state === "up") {
-              if (o.pressed) {
-                if (o.release) o.release();
-                o.pressed = false;
-                o.action = "released";
-              }
-
-              //Run the `out` method if it has been assigned
-              if (o.hoverOver) {
-                if (o.out) o.out();
-                o.hoverOver = false;
-              }
-            }
-          });
-        }
-      });
-    }
-
-    //A function that creates a sprite with 3 frames that
-    //represent the button states: up, over and down
-
-  }, {
-    key: "button",
-    value: function button(source) {
-      var x = arguments.length <= 1 || arguments[1] === undefined ? 0 : arguments[1];
-      var y = arguments.length <= 2 || arguments[2] === undefined ? 0 : arguments[2];
-
-      //The sprite object that will be returned
-      var o = undefined;
-
-      //Is it an array of frame ids or textures?
-      if (typeof source[0] === "string") {
-
-        //They're strings, but are they pre-existing texture or
-        //paths to image files?
-        //Check to see if the first element matches a texture in the
-        //cache
-        if (this.TextureCache[source[0]]) {
-
-          //It does, so it's an array of frame ids
-          o = this.MovieClip.fromFrames(source);
-        } else {
-
-          //It's not already in the cache, so let's load it
-          o = this.MovieClip.fromImages(source);
-        }
-      }
-
-      //If the `source` isn't an array of strings, check whether
-      //it's an array of textures
-      else if (source[0] instanceof this.Texture) {
-
-          //Yes, it's an array of textures.
-          //Use them to make a MovieClip o
-          o = new this.MovieClip(source);
-        }
-
-      //Add interactive properties to the button
-      this.makeInteractive(o);
-
-      //Set the `tinkType` to "button"
-      o.tinkType = "button";
-
-      //Position the button
-      o.x = x;
-      o.y = y;
-
-      //Return the new button sprite
-      return o;
-    }
-
-    //Run the `udpate` function in your game loop
-    //to update all of Tink's interactive objects
-
-  }, {
     key: "update",
     value: function update() {
 
       //Update the drag and drop system
       if (this.draggableSprites.length !== 0) this.updateDragAndDrop(this.draggableSprites);
-
-      //Update the buttons and button-like interactive sprites
-      if (this.buttons.length !== 0) this.updateButtons();
-    }
-
-    /*
-    `keyboard` is a method that listens for and captures keyboard events. It's really
-    just a convenient wrapper function for HTML `keyup` and `keydown` events so that you can keep your application code clutter-free and easier to write and read.
-     Here's how to use the `keyboard` method. Create a new keyboard object like this:
-    ```js
-    let keyObject = keyboard(asciiKeyCodeNumber);
-    ```
-    It's one argument is the ASCII key code number of the keyboard key
-    that you want to listen for. [Here's a list of ASCII key codes you can
-    use](http://www.asciitable.com).
-    Then assign `press` and `release` methods to the keyboard object like this:
-    ```js
-    keyObject.press = () => {
-      //key object pressed
-    };
-    keyObject.release = () => {
-      //key object released
-    };
-    ```
-    Keyboard objects also have `isDown` and `isUp` Boolean properties that you can use to check the state of each key. 
-    */
-
-  }, {
-    key: "keyboard",
-    value: function keyboard(keyCode) {
-      var key = {};
-      key.code = keyCode;
-      key.isDown = false;
-      key.isUp = true;
-      key.press = undefined;
-      key.release = undefined;
-
-      //The `downHandler`
-      key.downHandler = function (event) {
-        if (event.keyCode === key.code) {
-          if (key.isUp && key.press) key.press();
-          key.isDown = true;
-          key.isUp = false;
-        }
-        event.preventDefault();
-      };
-
-      //The `upHandler`
-      key.upHandler = function (event) {
-        if (event.keyCode === key.code) {
-          if (key.isDown && key.release) key.release();
-          key.isDown = false;
-          key.isUp = true;
-        }
-        event.preventDefault();
-      };
-
-      //Attach event listeners
-      window.addEventListener("keydown", key.downHandler.bind(key), false);
-      window.addEventListener("keyup", key.upHandler.bind(key), false);
-
-      //Return the key object
-      return key;
-    }
-
-    //`arrowControl` is a convenience method for updating a sprite's velocity
-    //for 4-way movement using the arrow directional keys. Supply it
-    //with the sprite you want to control and the speed per frame, in
-    //pixels, that you want to update the sprite's velocity
-
-  }, {
-    key: "arrowControl",
-    value: function arrowControl(sprite, speed) {
-
-      if (speed === undefined) {
-        throw new Error("Please supply the arrowControl method with the speed at which you want the sprite to move");
-      }
-
-      var upArrow = this.keyboard(38),
-          rightArrow = this.keyboard(39),
-          downArrow = this.keyboard(40),
-          leftArrow = this.keyboard(37);
-
-      //Assign key `press` methods
-      leftArrow.press = function () {
-        //Change the sprite's velocity when the key is pressed
-        sprite.vx = -speed;
-        sprite.vy = 0;
-      };
-      leftArrow.release = function () {
-        //If the left arrow has been released, and the right arrow isn't down,
-        //and the sprite isn't moving vertically:
-        //Stop the sprite
-        if (!rightArrow.isDown && sprite.vy === 0) {
-          sprite.vx = 0;
-        }
-      };
-      upArrow.press = function () {
-        sprite.vy = -speed;
-        sprite.vx = 0;
-      };
-      upArrow.release = function () {
-        if (!downArrow.isDown && sprite.vx === 0) {
-          sprite.vy = 0;
-        }
-      };
-      rightArrow.press = function () {
-        sprite.vx = speed;
-        sprite.vy = 0;
-      };
-      rightArrow.release = function () {
-        if (!leftArrow.isDown && sprite.vy === 0) {
-          sprite.vx = 0;
-        }
-      };
-      downArrow.press = function () {
-        sprite.vy = speed;
-        sprite.vx = 0;
-      };
-      downArrow.release = function () {
-        if (!upArrow.isDown && sprite.vx === 0) {
-          sprite.vy = 0;
-        }
-      };
     }
   }]);
 
   return Tink;
 })();
-//# sourceMappingURL=tink.js.map
